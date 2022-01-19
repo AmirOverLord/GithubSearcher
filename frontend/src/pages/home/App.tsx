c959af45c6ecf7a54e77377fabda2ebfce971fc8
Nova <overxlordxdev@gmail.com>
2022-01-19 19:58:00 +0400

    UI basic structure and add the react icons lib

--- frontend/src/pages/home/App.tsx
+++ frontend/src/pages/home/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import ResultList from "../../components/ResultList";
+import SearchInput from "../../components/SearchInput";
+import { GoMarkGithub } from "react-icons/go";
 import "./App.css";
 
 function App() {
-  return <>Working...</>;
+  return (
+    <div className="main">
+      <div className="top">
+        <GoMarkGithub className="github-icon" />
+        <div className="header">
+          <h1>GitHub Searcher</h1>
+          <h3>Search users or repos below</h3>
+        </div>
+      </div>
+
+      <SearchInput />
+
+      <ResultList />
+    </div>
+  );
 }
 
 export default App;
